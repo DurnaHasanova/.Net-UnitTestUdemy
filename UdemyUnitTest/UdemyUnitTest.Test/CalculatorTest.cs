37d5875df05fa3cd0e20133f9b36694d7bf1374e
DurnaHasanova <durna656@gmail.com>
2021-08-29 16:34:09 +0400

    Second

---
+++ UdemyUnitTest/UdemyUnitTest.Test/CalculatorTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UdemyUnitTest.APP;
+using Xunit;
+
+namespace UdemyUnitTest.Test
+{
+	public class CalculatorTest
+	{
+		[Fact]
+		public void AddTest()
+		{
+			// Arrange
+
+			int a = 5;
+			int b = 20;
+			var calculator = new Calculator();
+			//Act
+			var total = calculator.add(a, b);
+			//Assert
+			Assert.Equal<int>(25, total);
+		}
+	}
+}
