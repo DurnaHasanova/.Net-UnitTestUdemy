10895ede5902d8b6dfb90b2714321c3ae7fcea0d
DurnaHasanova <durna656@gmail.com>
2021-09-05 17:55:36 +0400

    Added Test

---
+++ UdemyRealWorldUnitTest/UdemyRealWorldUnitTest.TEST/ProductControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyRealWorldUnitTest.WEB.Controllers;
+using UdemyRealWorldUnitTest.WEB.Models;
+using UdemyRealWorldUnitTest.WEB.Repository;
+
+namespace UdemyRealWorldUnitTest.TEST
+{
+	public class ProductControllerTest
+	{
+		private readonly Mock<IRepository<Product>> _repositoryMock;
+		private readonly ProductsController _productController;
+
+		public ProductControllerTest()
+		{
+			_repositoryMock = new Mock<IRepository<Product>>();
+			_productController = new ProductsController(_repositoryMock.Object);
+		}
+	}
+}
