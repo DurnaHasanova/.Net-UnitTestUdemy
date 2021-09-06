312f0991bcbe93657fb782b7d7966a8a2ff3d38c
DurnaHasanova <durna656@gmail.com>
2021-09-06 23:04:26 +0400

    ApiControllertestClass

---
+++ UdemyRealWorldUnitTest/UdemyRealWorldUnitTest.TEST/ProductAPIcontrollerTest.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyRealWorldUnitTest.WEB.Controllers;
+using UdemyRealWorldUnitTest.WEB.Models;
+using UdemyRealWorldUnitTest.WEB.Repository;
+using Xunit;
+
+namespace UdemyRealWorldUnitTest.TEST
+{
+	public class ProductAPIcontrollerTest
+	{
+		private readonly Mock<IRepository<Product>> mockRepo;
+		private readonly ProductsAPIController aPIcontroller;
+		private List<Product> _products;
+
+		public ProductAPIcontrollerTest()
+		{
+			mockRepo = new Mock<IRepository<Product>>();
+			aPIcontroller = new ProductsAPIController(mockRepo.Object);
+			_products = new List<Product>() {
+				new Product { Id = 1, Name = null, Price = 10, Stock = 5, Color = "red" },
+				new Product { Id = 2, Name = "Test2", Price = 9, Stock = 15, Color = "yellow" },
+				new Product { Id = 3, Name = "Test3", Price = 5, Stock = 17, Color = "green" }};
+		}
+
+		[Fact]
+		public async void Get_ExecuteAction_ReturnOkStatusCodeAndProductList()
+		{
+			mockRepo.Setup(x => x.GetAll()).ReturnsAsync(_products);
+
+			var result = await aPIcontroller.GetProducts();
+
+			var okResult = Assert.IsType<OkObjectResult>(result);
+
+			var returnProducts = Assert.IsAssignableFrom<IEnumerable<Product>>(okResult.Value);
+			Assert.Equal(_products.Count, returnProducts.Count());
+		}
+
+		[Theory]
+		[InlineData(5)]
+		public async void Get_WithNullId_ReturnNotFound(int id)
+		{
+			Product product = null;
+			mockRepo.Setup(x => x.GetById(id)).ReturnsAsync(product);
+
+			var result = await aPIcontroller.GetProduct(id);
+
+			var okResult = Assert.IsType<NotFoundResult>(result);
+		}
+
+		[Theory]
+		[InlineData(2)]
+		public async void Get_WithId_ReturnOkStatusCodeAndProduct(int id)
+		{
+			Product product = _products.FirstOrDefault(x => x.Id == id);
+			mockRepo.Setup(x => x.GetById(id)).ReturnsAsync(product);
+
+			var result = await aPIcontroller.GetProduct(id);
+
+			var okResult = Assert.IsType<OkObjectResult>(result);
+			var returnProducts = Assert.IsType<Product>(okResult.Value);
+			Assert.Equal(product, returnProducts);
+		}
+
+		[Theory]
+		[InlineData(2, 3)]
+		public async void Put_IdDoesnotMatchWithProductId_ReturnBadRequest(int id, int invalidId)
+		{
+			Product product = _products.FirstOrDefault(x => x.Id == invalidId);
+			var result = await aPIcontroller.PutProduct(id, product);
+
+			var okResult = Assert.IsType<BadRequestResult>(result);
+		}
+
+		[Theory]
+		[InlineData(2)]
+		public async void Put_IdMatchesWithProductId_ReturnNoContent(int id)
+		{
+			Product product = _products.FirstOrDefault(x => x.Id == id);
+			mockRepo.Setup(x => x.Update(product));
+
+			var result = await aPIcontroller.PutProduct(id, product);
+
+			var okResult = Assert.IsType<NoContentResult>(result);
+			mockRepo.Verify(repo => repo.Update(It.IsAny<Product>()), Times.Once);
+		}
+
+		[Fact]
+		public async void Post_ExecuteAction_ReturnCreateAction()
+		{
+			Product product = new Product { Name = "test", Price = 1, Stock = 1, Color = "red" };
+			mockRepo.Setup(x => x.Create(product));
+
+			var result = await aPIcontroller.PostProduct(product);
+
+			var okResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+			var size = okResult.RouteValues.Count;
+			Assert.Equal(1, size);
+			Assert.Equal("GetProduct", okResult.ActionName);
+			Assert.True(okResult.RouteValues.ContainsKey("id"));
+
+			mockRepo.Verify(repo => repo.Create(It.IsAny<Product>()), Times.Once);
+		}
+
+		[Theory]
+		[InlineData(5)]
+		public async void Delete_WithNullId_ReturnNotFound(int id)
+		{
+			Product product = null;
+			mockRepo.Setup(x => x.GetById(id)).ReturnsAsync(product);
+
+			var result = await aPIcontroller.DeleteProduct(id);
+
+			var okResult = Assert.IsType<NotFoundResult>(result);
+		}
+
+		[Theory]
+		[InlineData(2)]
+		public async void Delete_WithId_ReturnNoContent(int id)
+		{
+			Product product = _products.FirstOrDefault(x => x.Id == id);
+			mockRepo.Setup(x => x.GetById(id)).ReturnsAsync(product);
+
+			var result = await aPIcontroller.DeleteProduct(id);
+
+			var okResult = Assert.IsType<NoContentResult>(result);
+			mockRepo.Verify(repo => repo.Delete(It.IsAny<Product>()), Times.Once);
+		}
+	}
+}
---
+++ UdemyRealWorldUnitTest/UdemyRealWorldUnitTest.WEB/Controllers/ProductsAPIController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UdemyRealWorldUnitTest.WEB.Models;
+using UdemyRealWorldUnitTest.WEB.Repository;
+
+namespace UdemyRealWorldUnitTest.WEB.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsAPIController : ControllerBase
+	{
+		private readonly IRepository<Product> repository;
+
+		public ProductsAPIController(IRepository<Product> repository)
+		{
+			this.repository = repository;
+		}
+
+		// GET: api/ProductsAPI
+		[HttpGet]
+		public async Task<IActionResult> GetProducts()
+		{
+			var data = await repository.GetAll();
+			return Ok(data);
+		}
+
+		// GET: api/ProductsAPI/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult> GetProduct(int id)
+		{
+			var product = await repository.GetById(id);
+
+			if (product == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(product);
+		}
+
+		// PUT: api/ProductsAPI/5
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutProduct(int id, Product product)
+		{
+			if (id != product.Id)
+			{
+				return BadRequest();
+			}
+
+			await repository.Update(product);
+
+			return NoContent();
+		}
+
+		// POST: api/ProductsAPI
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPost]
+		public async Task<ActionResult<Product>> PostProduct(Product product)
+		{
+			await repository.Create(product);
+
+			return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+		}
+
+		// DELETE: api/ProductsAPI/5
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteProduct(int id)
+		{
+			var product = await repository.GetById(id);
+			if (product == null)
+			{
+				return NotFound();
+			}
+
+			repository.Delete(product);
+
+			return NoContent();
+		}
+
+		private async Task<bool> ProductExists(int id)
+		{
+			var product = await repository.GetById(id);
+			return product == null ? false : true;
+		}
+	}
+}
